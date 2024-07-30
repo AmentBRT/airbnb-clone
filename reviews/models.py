9b438fcc1aabf37919c635819c9abd7c3a5dc4ee
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-07-30 23:46:37 +0330

    Create Review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from core import models as core_models
+from users import models as users_models
+from rooms import models as rooms_models
 
-# Create your models here.
+
+class Review(core_models.TimeStampedModel):
+    """Review Model"""
+
+    user = models.ForeignKey(users_models.User, on_delete=models.SET_NULL, null=True)
+    room = models.ForeignKey(rooms_models.Room, on_delete=models.CASCADE)
+    review = models.TextField()
+    accuracy = models.PositiveSmallIntegerField()
+    communication = models.PositiveSmallIntegerField()
+    cleanliness = models.PositiveSmallIntegerField()
+    location = models.PositiveSmallIntegerField()
+    check_in = models.PositiveSmallIntegerField()
+    value = models.PositiveSmallIntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.review} - {self.room}'
