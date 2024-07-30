df68fc240605f4bd05f362e243a0849adebc5129
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-07-30 14:24:03 +0330

    Create User model which extends AbstractUser with more fields

--- users/models.py
+++ users/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Custom User Model"""
+
+    class Gender(models.TextChoices):
+        MALE = 'm', 'Male'
+        FEMALE = 'f', 'Female'
+        OTHER = 'o', 'Other'
+
+    class Language(models.TextChoices):
+        ENGLISH = 'en', 'English'
+        PERSIAN = 'fa', 'Persian'
+
+    class Currency(models.TextChoices):
+        USD = 'usd', 'USD'
+        IRR = 'irr', 'Iran Rial'
+
+    avatar = models.ImageField(null=True, blank=True, default=None)
+    gender = models.CharField(
+        max_length=1,
+        choices=Gender,
+        null=True,
+        blank=True,
+        default=None,
+    )
+    bio = models.TextField(null=True, blank=True, default=None)
+    birthdate = models.DateField(null=True, blank=True, default=None)
+    language = models.CharField(
+        max_length=2,
+        choices=Language,
+        null=True,
+        blank=True,
+        default=None,
+    )
+    currency = models.CharField(
+        max_length=3,
+        choices=Currency,
+        null=True,
+        blank=True,
+        default=None,
+    )
+    superhost = models.BooleanField(default=False)
