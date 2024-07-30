c2067b70fade4c9bf5c4e1eee55b2ac408663d83
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-07-30 23:26:26 +0330

    Register RoomType, Amenity, Facility, HouseRule, Room and Photo models

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.RoomType)
+class RoomTypeAdmin(admin.ModelAdmin):
+    """Room Type Admin Model"""
+
+
+@admin.register(models.Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    """Amenity Admin Model"""
+
+
+@admin.register(models.Facility)
+class FacilityAdmin(admin.ModelAdmin):
+    """Facility Admin Model"""
+
+
+@admin.register(models.HouseRule)
+class HouseRule(admin.ModelAdmin):
+    """House Rule Admin Model"""
+
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+    """Room Admin Model"""
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """Photo Admin Model"""
