564f9fa64dd079cbbd67e1c808412f0109d512ba
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-07-30 23:25:15 +0330

    Create AbstractItem abstract model and RoomType, Amenity, Facility, HouseRule, Room and Photo models

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django_countries import fields as countries_fields
+from core import models as core_models
+from users import models as users_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+    """Abstract Item Model"""
+
+    name = models.CharField(max_length=80)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        abstract = True
+
+
+class RoomType(AbstractItem):
+    """Room Type Model"""
+
+    class Meta:
+        verbose_name = 'Room Type'
+
+
+class Amenity(AbstractItem):
+    """Amenity Model"""
+
+    class Meta:
+        verbose_name_plural = 'Amenities'
+
+
+class Facility(AbstractItem):
+    """Facility Model"""
+
+    class Meta:
+        verbose_name_plural = 'Facilities'
+
+
+class HouseRule(AbstractItem):
+    """House Rule Model"""
+
+    class Meta:
+        verbose_name = 'House Rule'
+
+
+class Room(core_models.TimeStampedModel):
+    """Room Model"""
+
+    host = models.ForeignKey(users_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = countries_fields.CountryField()
+    city = models.CharField(max_length=80)
+    price = models.PositiveIntegerField()
+    address = models.CharField(max_length=140)
+    guests = models.PositiveSmallIntegerField()
+    beds = models.PositiveSmallIntegerField()
+    bedrooms = models.PositiveSmallIntegerField()
+    baths = models.PositiveSmallIntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Photo(core_models.TimeStampedModel):
+    """Photo Model"""
+
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+
+    def __str__(self) -> str:
+        return self.caption
