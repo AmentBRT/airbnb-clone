ab0a5e0d7c14894564ce24cd2f162f96edc7b6f8
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-07-31 02:08:40 +0330

    Customize admin model for Room model

--- rooms/admin.py
+++ rooms/admin.py
@@ -26,6 +26,67 @@ class HouseRule(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """Room Admin Model"""
 
+    fieldsets = (
+        (
+            'Basic Info',
+            {'fields': ('name', 'description', 'country', 'city', 'price', 'address')},
+        ),
+        (
+            'Times',
+            {'fields': ('check_in', 'check_out', 'instant_book')},
+        ),
+        (
+            'Spaces',
+            {'fields': ('guests', 'beds', 'bedrooms', 'baths')},
+        ),
+        (
+            'More about the space',
+            {
+                'classes': ('collapse',),
+                'fields': ('amenities', 'facilities', 'house_rules'),
+            },
+        ),
+        (
+            'Last details',
+            {'fields': ('host',)},
+        ),
+    )
+    list_display = [
+        'name',
+        'country',
+        'city',
+        'price',
+        'guests',
+        'beds',
+        'bedrooms',
+        'baths',
+        'check_in',
+        'check_out',
+        'instant_book',
+        'amenities_count',
+        'photos_count',
+    ]
+    list_filter = [
+        'instant_book',
+        'host__superhost',
+        'room_type',
+        'amenities',
+        'facilities',
+        'house_rules',
+        'city',
+        'country',
+    ]
+    search_fields = ['city', '^host__username']
+    filter_horizontal = ['amenities', 'facilities', 'house_rules']
+
+    @admin.display(ordering='amenities__count')
+    def amenities_count(self, obj: models.Room):
+        return obj.amenities.count()
+
+    @admin.display(ordering='photos__count')
+    def photos_count(self, obj: models.Room):
+        return obj.photos.count()
+
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
