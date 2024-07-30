182999d1b71fcd3aa7ea65747898412cb7c5b57f
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-07-30 14:25:34 +0330

    Register UserAdmin model admin which extends UserAdmin with User model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Custom User Admin Model"""
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (
+            'Custom Profile',
+            {
+                'fields': (
+                    'avatar',
+                    'gender',
+                    'bio',
+                    'birthdate',
+                    'language',
+                    'currency',
+                    'superhost',
+                )
+            },
+        ),
+    )
