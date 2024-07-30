cc4f7aa9abddedd041edc12b49744554b694dd33
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-07-31 00:11:45 +0330

    Create List model

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from core import models as core_models
+from rooms import models as rooms_models
+from users import models as users_models
 
-# Create your models here.
+
+class List(core_models.TimeStampedModel):
+    """List Model"""
+
+    user = models.ForeignKey(users_models.User, on_delete=models.CASCADE)
+    rooms = models.ManyToManyField(rooms_models.Room, blank=True)
+    name = models.CharField(max_length=80)
+
+    def __str__(self) -> str:
+        return self.name
