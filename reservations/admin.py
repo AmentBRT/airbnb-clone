342ab2491d806487704773f5fcc7228b2def998c
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-08-01 12:46:05 +0330

    Add list display and list filter to and set boolean=True for in progress and is finished methods in ReservationAdmin admin model

--- reservations/admin.py
+++ reservations/admin.py
@@ -5,3 +5,22 @@ from . import models
 @admin.register(models.Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     """Reservation Admin Model"""
+
+    list_display = [
+        'room',
+        'status',
+        'check_in',
+        'check_out',
+        'guest',
+        'in_progress',
+        'is_finished',
+    ]
+    list_filter = ['status']
+
+    @admin.display(boolean=True)
+    def in_progress(self, obj: models.Reservation) -> bool:
+        return obj.in_progress()
+
+    @admin.display(boolean=True)
+    def is_finished(self, obj: models.Reservation) -> bool:
+        return obj.is_finished()
