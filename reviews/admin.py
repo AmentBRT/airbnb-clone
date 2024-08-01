29e180feab754ac5c3cd40d3ec74b19784dc47e9
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-08-01 12:06:15 +0330

    Add rating average to list display and change its description to 'AVG.'

--- reviews/admin.py
+++ reviews/admin.py
@@ -5,3 +5,9 @@ from . import models
 @admin.register(models.Review)
 class ReviewAdmin(admin.ModelAdmin):
     """Review Admin Model"""
+
+    list_display = ['__str__', 'rating_average']
+
+    @admin.display(description='AVG.')
+    def rating_average(self, obj: models.Review) -> float:
+        return obj.rating_average()
