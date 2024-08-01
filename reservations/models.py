697768408a4a37bfc561927dd031b481edff1867
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-08-01 12:44:22 +0330

    Add in progress and is finished mehods to Reservation model

--- reservations/models.py
+++ reservations/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from core import models as core_models
 from rooms import models as rooms_models
 from users import models as users_models
@@ -25,6 +26,14 @@ class Reservation(core_models.TimeStampedModel):
     status = models.CharField(max_length=2, choices=Status, default=Status.PENDING)
     check_in = models.DateField()
     check_out = models.DateField()
+
+    def in_progress(self) -> bool:
+        now = timezone.now().date()
+        return self.check_in <= now <= self.check_out
+
+    def is_finished(self) -> bool:
+        now = timezone.now().date()
+        return now > self.check_out
 
     def __str__(self) -> str:
         return f'{self.room} - {self.check_in}'
